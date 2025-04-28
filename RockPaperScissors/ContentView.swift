49b1e6ca7e9d90f9741f228b45d0bd74828b1f33
Mohamed Obaya <mohamed.ali.obaya@gmail.com>
2025-04-28 20:32:02 +0300

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Mohamed Obaya on 28/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
